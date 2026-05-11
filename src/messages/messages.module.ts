9202d4b3dfbf940c6230057b3595e1af99ca9dde
MKfery <ferytail360@gmail.com>
2026-05-11 11:59:02 +0000

    commit

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { EventsModule } from '../events/events.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EventsModule],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
